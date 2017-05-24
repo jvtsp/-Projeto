cf389ba209983e7893f92b24bcea82b742fb0b21
root <root@jessie.raw>
2017-05-24 13:15:57 +0000

    loih

--- usuario.sh
+++ usuario.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 function GCU(){
-user=$( dialog --stdout --title "Novo usuario" --inputbox 'Digite o nome desejado: ' 0 0 )
-senha=$( dialog --stdout --title "Digite a senha" --inputbox 'Digite a senha' 0 0)
-	mkdir /home/$user
-	adduser $user -p $senha -d/home/$user
+USUARIO=$( dialog --stdout --title "Novo usuario" --inputbox 'Digite o nome desejado: ' 0 0 )
+	useradd $USUARIO
+[ $? == 0 ] && (dialog --msgbox "criado com sucesso" 0 0; \
+ mkdir /home/$USUARIO ; OPCAO) || (dialog --msgbox "tente novamente" 0 0; GCU)
 }
 function APU(){
- dialog --stdout --title "Apagar Usuario" --inputbox 'Digite o usuario que deseja apagar'
+DELU=$( dialog --stdout --title "Apagar Usuario" --inputbox 'Digite o usuario desejado: ' 0 0 )
+	userdel $DELU
+[ $? == 0 ] && (dialog --msgbox "apagado com sucesso" 0 0; \
+ rmdir /home/$DELU ; OPCAO) || (dialog --msgbox "tente novamente" 0 0; APU)
 }
-GCU
+function CAG(){
+NEWG=$( dialog --stdout --title "Novo Grupo" --inputbox 'Digite o grupo desejado: ' 0 0 )
+	
+[ $? == 0 ] && (dialog --msgbox "criado com sucesso" 0 0; \
+  ; OPCAO) || (dialog --msgbox "tente novamente" 0 0; CAG)
 }
-OPCAO=$(dialog							\
+
+function OPCAO(){
+MENU=$(dialog							\
 	--stdout						\
 	--title "Gerenciador de usuarios"			\
 	--menu "Escolha uma opção: "				\
@@ -22,7 +31,7 @@ OPCAO=$(dialog							\
 	5 "Listar /etc/passwd"					\
 	6 "voltar")
 
-	case $OPCAO in
+	case $MENU in
 		1) GCU 	   ;;
 		2) APU     ;;
 		3) CAG     ;;
@@ -31,5 +40,5 @@ OPCAO=$(dialog							\
 		6) bash /home/vagrant/menu.sh ;;
 		*) dialog --title "Opçao invalida." --msgbox "Digite Novamente" 0 0 ;;
 	esac
-
-
+}
+OPCAO
